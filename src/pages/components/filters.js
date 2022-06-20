64c26b9ecbbc3fd141b2dda49ae5c6fe2ed525d3
Nicu Savin <savinicu@gmail.com>
2022-06-20 18:34:50 +0100

    filters remodel

--- src/pages/components/filters.js
+++ src/pages/components/filters.js
@@ -11,62 +11,44 @@ import React, { useState } from "react";
 function Filters({ handleClick }) {
   const [filters, setFilters] = useState({
     keywords: null,
-    popularity: "Don't apply",
-    revenue: "Don't apply",
+    popularity: "popularity.desc",
     adult: false,
   });
 
-  const popularityFilter = (e) => {
-    setFilters({ ...filters, popularity: e.target.value });
-  };
-
-  const revenueFilter = (e) => {
-    setFilters({ ...filters, revenue: e.target.value });
-  };
-
-  const adultFilter = (e) => {
-    setFilters({ ...filters, adult: e.target.value });
-  };
-
-  const handleChange = (e) => {
-    setFilters({
-      ...filters,
-      keywords: e.target.value,
-    });
-  };
-
   return (
     <div className="filters">
-      <TextField onChange={handleChange} label="Movie keywords"></TextField>
+      <TextField
+        onChange={(e) => {
+          setFilters({
+            ...filters,
+            keywords: e.target.value,
+          });
+        }}
+        label="Keywords"
+      ></TextField>
       <FormControl className="filter">
         <InputLabel>Popularity</InputLabel>
         <Select
           value={filters.popularity}
           label="Popularity"
-          onChange={popularityFilter}
+          onChange={(e) => {
+            setFilters({ ...filters, popularity: e.target.value });
+          }}
         >
-          <MenuItem value="Don't apply">Don't apply</MenuItem>
-          <MenuItem value="popularity.asc">Ascending</MenuItem>
           <MenuItem value="popularity.desc">Descending</MenuItem>
-        </Select>
-      </FormControl>
-
-      <FormControl className="filter">
-        <InputLabel>Revenue</InputLabel>
-        <Select
-          value={filters.revenue}
-          label="Revenue"
-          onChange={revenueFilter}
-        >
-          <MenuItem value="Don't apply">Don't apply</MenuItem>
-          <MenuItem value="revenue.asc">Ascending</MenuItem>
-          <MenuItem value="revenue.desc">Descending</MenuItem>
+          <MenuItem value="popularity.asc">Ascending</MenuItem>
         </Select>
       </FormControl>
 
       <FormControl className="filter">
         <InputLabel>Adult</InputLabel>
-        <Select value={filters.adult} label="Revenue" onChange={adultFilter}>
+        <Select
+          value={filters.adult}
+          label="Adult"
+          onChange={(e) => {
+            setFilters({ ...filters, adult: e.target.value === "true" });
+          }}
+        >
           <MenuItem value="false">Don't include</MenuItem>
           <MenuItem value="true">Include</MenuItem>
         </Select>
