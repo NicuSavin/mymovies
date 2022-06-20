5ea3ad74b8662b16880f885d32e6d360675242d5
Nicu Savin <savinicu@gmail.com>
2022-06-20 12:12:38 +0100

    added routing. created page files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import TopBar from "./topBar";
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/main";
+import MoviesPage from "./pages/movies";
 function App() {
   return (
     <div>
       <TopBar />
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/home" />} />
+        <Route exact path="/home" element={<MainPage />}></Route>
+        <Route exact path="/movies" element={<MoviesPage />}></Route>
+        <Route exact path="/series" element={<MainPage />}></Route>
+        <Route exact path="/celebs" element={<MainPage />}></Route>
+        <Route exact path="/browse" element={<MainPage />}></Route>
+      </Routes>
     </div>
   );
 }
