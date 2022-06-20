dfd546bd6e684a3c3bdf47edbc6397c2c613ceac
Nicu Savin <savinicu@gmail.com>
2022-06-20 18:36:52 +0100

    added celeb and series page to coresponding routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import TopBar from "./topBar";
 import { Navigate, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/main";
 import MoviesPage from "./pages/movies";
+import SeriesPage from "./pages/series";
+import CelebsPage from "./pages/celebs";
 function App() {
   return (
     <div>
@@ -10,8 +12,8 @@ function App() {
         <Route exact path="/" element={<Navigate to="/home" />} />
         <Route exact path="/home" element={<MainPage />}></Route>
         <Route exact path="/movies" element={<MoviesPage />}></Route>
-        <Route exact path="/series" element={<MainPage />}></Route>
-        <Route exact path="/celebs" element={<MainPage />}></Route>
+        <Route exact path="/series" element={<SeriesPage />}></Route>
+        <Route exact path="/celebs" element={<CelebsPage />}></Route>
         <Route exact path="/browse" element={<MainPage />}></Route>
       </Routes>
     </div>
