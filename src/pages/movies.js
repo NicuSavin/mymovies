e12c23aaed1bc953405f3b3818f58dd7f0de2417
Nicu Savin <savinicu@gmail.com>
2022-06-21 13:37:44 +0100

    movies page functionality

--- src/pages/movies.js
+++ src/pages/movies.js
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Filters from "./components/filters";
-
+import { discoverMovies, searchMovies } from "./components/utils";
+import DisplayCard from "./components/displayCard";
+import { Grid } from "@mui/material";
 const MoviesPage = () => {
-  return <Filters handleClick={handleClick} />;
+  const [movies, setMovies] = useState({ show: false, list: [] });
+
+  async function handleClick(obj) {
+    let searchData = null;
+    if (obj.keywords !== null) {
+      searchData = await searchMovies(obj);
+    }
+    if (searchData === null) {
+      searchData = await discoverMovies(obj);
+    } else {
+      //sorting by popularity filter cause the API can't sort if searching (different endpoints) :(
+      if (searchData.results) {
+        if (obj.popularity === "popularity.desc") {
+          searchData.results.sort(function (a, b) {
+            return b.popularity - a.popularity;
+          });
+        } else {
+          searchData.results.sort(function (a, b) {
+            return a.popularity - b.popularity;
+          });
+        }
+        setMovies({ show: true, list: searchData.results });
+      }
+    }
+  }
+
+  return (
+    <>
+      <Filters handleClick={handleClick} />
+      {movies.show ? (
+        <Grid container spacing={2}>
+          {movies.list.map(function (movie, i) {
+            if (movie) {
+              return (
+                <DisplayCard
+                  key={i}
+                  card={{
+                    title: movie.original_title,
+                    overview: movie.overview,
+                    button: true,
+                    img1: movie.backdrop_path,
+                    img2: movie.poster_path,
+                  }}
+                />
+              );
+            }
+          })}
+        </Grid>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 };
 
 export default MoviesPage;
