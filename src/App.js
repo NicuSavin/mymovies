e7280882c89c9dd59dfa3e8700b9a7b67bdb0e57
Nicu Savin <savinicu@gmail.com>
2022-11-16 14:54:42 +0100

    removed browse route. added PageNotFound route. local storage fix

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import TopBar from "./pages/components/TopBar";
+import TopBar from "./pages/components/topBar";
 import { Navigate, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/main";
 import MoviesPage from "./pages/movies";
 import SeriesPage from "./pages/series";
 import CelebsPage from "./pages/celebs";
-import MyList from "./pages/mylist";
-import { useEffect } from "react";
+import PageNotFound from "./pages/components/pageNotFound";
 function App() {
-  if (!localStorage.getItem("watched")) {
-    localStorage.setItem("watched", []);
-  }
-
   if (!localStorage.getItem("watchLater")) {
-    localStorage.setItem("watched", []);
+    localStorage.setItem(
+      "watchLater",
+      JSON.stringify({ movies: [], series: [] })
+    );
   }
-
   return (
     <div>
       <TopBar />
       <Routes>
+        <Route exact path="*" element={<PageNotFound />}></Route>
         <Route exact path="/" element={<Navigate to="/home" />} />
         <Route exact path="/home" element={<MainPage />}></Route>
         <Route exact path="/movies" element={<MoviesPage />}></Route>
         <Route exact path="/series" element={<SeriesPage />}></Route>
         <Route exact path="/celebs" element={<CelebsPage />}></Route>
-        <Route exact path="/browse" element={<MainPage />}></Route>
         <Route exact path="/mylist" element={<MyList />}></Route>
       </Routes>
     </div>
